ef82a7147de188a592f3355df33e1f16635002a3
jacopomazzo <114420544+jacopomazzo@users.noreply.github.com>
2022-11-17 14:26:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+   float a = 0, b = 0;
+    int opz = 0;
+    cin>>a>>b;
+    cin>>opz;
+    switch (opz) {
+        case 0:
+            cout<<(a*b)/2<<endl;
+            break;
+        case 1:
+            cout<<a*a<<endl;
+            break;
+        case 2:
+            cout<<a*b<<endl;
+            break;
+        default:
+            cout<<"opzione non valida";
+    }
    return 0;
 }
